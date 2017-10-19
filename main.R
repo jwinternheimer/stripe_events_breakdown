797f2bbb30fcdd134d353f34cb2ac93a5962c7c1
jwinternheimer <julianwinternheimer@gmail.com>
2017-10-19 17:18:13 -0400

    Better handling of discounts. Created an aggregated customer discounts dataframe and removed duplicates.

--- main.R
+++ main.R
@@ -57,60 +57,17 @@ main <- function(start, end) {
   # apply discounts
   events_discounted <- apply_discount(events_with_discounts)
   
+  # replace na with 0
   events_discounted$discounted_amount[is.na(events_discounted$discounted_amount)] <- 0
   
+  # subtract discounts from MRR
   events_discounted <- events_discounted %>%
     mutate(mrr_amount = mrr_amount - discounted_amount)
   
-  # remove duplicates
-  #events_discounted <- events_discounted %>%
-  #  group_by(id) %>%
-  #  arrange(mrr_amount) %>%
-  #  filter(row_number() == 1) %>%
-  #  ungroup()
   
-  return(mrr_changes)
+  return(events_discounted)
 }
 
 
-
-# set start date
-start <- '2017-09-01'
-end <- '2017-09-30'
-
 # get september events
-september <- main(start, end)
-
-# get august events
-august <- main(start = '2017-08-01', end = '2017-08-31')
-
-# sum events
-september %>%
-  filter(event_type != 'other') %>%
-  group_by(event_type) %>%
-  summarise(mrr = sum(mrr_amount))
-
-# sum events
-august %>%
-  filter(event_type != 'other') %>%
-  group_by(event_type) %>%
-  summarise(mrr = sum(mrr_amount))
-
-
-
-
-# read data
-april <- readRDS('april_events.rds')
-may <- readRDS('may_events.rds')
-june <- readRDS('june.rds')
-july <- readRDS('july_events.rds')
-august <- readRDS('august_events.rds')
-september <- readRDS('september_events.rds')
-october <- readRDS('october_events.rds')
-
-# bind events
-all_events <- rbind(april, may, june, july, august, september, october)
-events_parsed <- all_events
-
-# save
-saveRDS(events_discounted, file = '~/Documents/stripe-mrr-breakdown/april_oct.rds')
+# september <- main(start = '2017-09-01', end = '2017-09-30')
